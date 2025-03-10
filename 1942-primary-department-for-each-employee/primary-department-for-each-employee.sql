b313c49d9b1bc9acb1e1135f1ffcb6413a16109b
Varun Sonawane <varunsonawane23@gmail.com>
2025-03-10 09:35:43 -0600

    Time: 585 ms (91.77%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -3,10 +3,11 @@ select employee_id, department_id
 from Employee m
 where primary_flag = "Y"
 
-union
+OR
 
-select employee_id, department_id
-from employee em
-where employee_id not in (select employee_id
-                          from employee
-                          where primary_flag = "Y")
+employee_id in
+(select employee_id
+from employee
+group by employee_id
+having count(employee_id) = 1
+)
