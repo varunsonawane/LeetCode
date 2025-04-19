aaf6c83e00209ede571c5a33a3b57d0d7b279499
Varun Sonawane <varunsonawane23@gmail.com>
2025-04-19 14:42:08 -0400

    Time: 1189 ms (88.06%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select distinct u.user_id as buyer_id, u.join_date,
+                count(case when year(o.order_date) = 2019 then o.order_id end) as orders_in_2019
+from users u left join orders o
+on u.user_id = o.buyer_id
+group by u.user_id, u.join_date
+
+
+
+
