891c9e651412b00c65fdb222051003958d8a2b6f
Varun Sonawane <varunsonawane23@gmail.com>
2025-04-19 14:42:42 -0400

    Time: 1163 ms (92.07%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -3,7 +3,7 @@ select distinct u.user_id as buyer_id, u.join_date,
                 count(case when year(o.order_date) = 2019 then o.order_id end) as orders_in_2019
 from users u left join orders o
 on u.user_id = o.buyer_id
-group by u.user_id, u.join_date
+group by u.user_id, u.join_date;
 
 
 
